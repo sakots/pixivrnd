a6050d84921449bbdb85dbfafea025f40a17d919
Pocala <34614432+pocala@users.noreply.github.com>
2021-06-27 02:22:14 +0900

    デザインを変更。10回連続で飛ぶボタンを追加。 ランダムに飛ぶ範囲を設定できるようにした。

---
+++ main.js
@@ -0,0 +1,43 @@
+function swap(a, b) {
+  var c = b;
+  b = a;
+  a = c;
+}
+
+function randomURL(minid, maxid) {
+  if (minid < maxid) swap(minid, maxid);
+  return 'https://www.pixiv.net/artworks/' + (Math.floor(Math.random() * (maxid + 1 - minid)) + minid);
+}
+
+// ==============以下スライダー設定===============
+
+const minIdInput = document.getElementById('minID_range');
+const maxIdInput = document.getElementById('maxID_range');
+const minValueTarget = document.getElementById('current_minID_value');
+const maxValueTarget = document.getElementById('current_maxID_value');
+
+// 初期化
+minValueTarget.innerHTML = minIdInput.value;
+maxValueTarget.innerHTML = maxIdInput.value;
+
+var rangeValue = function(source, target) {
+  return function(evt) {
+    target.innerHTML = source.value;
+  }
+}
+
+// inputが何か動いたらそれに反応して値を書き換える
+minIdInput.addEventListener('input', rangeValue(minIdInput, minValueTarget));
+maxIdInput.addEventListener('input', rangeValue(maxIdInput, maxValueTarget));
+
+// =============================================
+
+function pixivrnd() {
+  window.open(randomURL(Number(minIdInput.value), Number(maxIdInput.value)));
+}
+
+function pixivrnd_10() {
+  for (i = 0; i < 10; i++) {
+    pixivrnd();
+  }
+}
